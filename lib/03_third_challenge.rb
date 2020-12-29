dde5696c331b5e8233610734f90f546a078e3846
sydney miranda <sydmiranda@icloud.com>
2020-12-29 00:35:28 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -13,7 +13,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -28,5 +28,5 @@ def third_challenge
    }
   }
 
-  
+
 end
