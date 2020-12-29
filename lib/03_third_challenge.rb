2934d501f0a1460706a24513b802fd0288729fa3
sydney miranda <sydmiranda@icloud.com>
2020-12-29 00:36:22 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -8,9 +8,14 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
         #your key/value pairs here
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         },
       :hero_friends => []
    },
